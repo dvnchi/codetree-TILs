df26fb93dd8712ee91353f9d97ea167dd6be75a5
dvnchi <44892620+dvnchi@users.noreply.github.com>
2026-02-14 14:07:15 +0900

    Title: 선택 정렬 구현, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260214/선택 정렬 구현/implement-selection-sort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[100];
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Please write your code here.
+    int minimum;
+    int tmp;
+    for(int i=0; i<n-1; i++){
+        minimum = i;
+        for(int j=i+1; j<n; j++){
+            if(arr[j] < arr[minimum]){
+                minimum = j;
+            }
+        }
+        tmp = arr[i];
+        arr[i] = arr[minimum];
+        arr[minimum] = tmp;
+        
+    }
+
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
